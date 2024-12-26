9bd2851b374f80bf828073f1711664d3415ba3d1
cmcav <78579382+cmcav@users.noreply.github.com>
2024-12-25 22:53:25 -0500

    Add files via upload
    *Updated titling, description

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { Nav } from "@/components/Nav";
 import { cn } from "@/utils";
 
 export const metadata: Metadata = {
-  title: "Hume AI - EVI - Next.js Starter",
-  description: "A Next.js starter using Hume AI's Empathic Voice Interface",
+  title: "Solace AI - Revolutionizing Communication.",
+  description: "Bridging Emotions and Technology for Deeper Connections.",
 };
 
 export default function RootLayout({
